64cd6156f736b675218011297cf7b1e2416a88e3
Nate Goldman <nate@ngoldman.me>
2015-07-06 16:11:47 -0700

    :art: modify script to convert all markdown docs to html

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var glob = require('glob')
 var md = require('markdown-it')({
   html: true,
   linkify: true,
@@ -7,25 +8,34 @@ var md = require('markdown-it')({
 var opts = { encoding: 'utf8' }
 var header = fs.readFileSync('./partials/_header.html', opts)
 var footer = fs.readFileSync('./partials/_footer.html', opts)
-var pages = []
 
-pages.push({
+// add root README as index
+var pages = [{
   path: 'index.html',
   body: md.render(fs.readFileSync('./README.md', opts))
-}, {
-  path: 'docs/index.html',
-  body: md.render(fs.readFileSync('./docs/README.md', opts))
-}, {
-  path: 'docs/setup.html',
-  body: md.render(fs.readFileSync('./docs/setup.md', opts))
-}, {
-  path: 'docs/heroku.html',
-  body: md.render(fs.readFileSync('./docs/heroku.md', opts))
+}]
+
+// add all markdown docs
+glob.sync('docs/**/*.md').forEach(function (file) {
+  var path = file.split('/').slice(0, -1).join('/')
+  var name = file.split('.')[0].split('/').pop()
+
+  // treat README as directory index
+  if (name === 'README') name = 'index'
+
+  pages.push({
+    path: path + '/' + name + '.html',
+    body: md.render(fs.readFileSync('./' + file, opts))
+  })
 })
 
+// take all pages and convert from markdown to html
 pages.forEach(function (page) {
-  var rewrite = page.body.replace(/\.md"/g, '.html"')
-  fs.writeFile(page.path, header + rewrite + footer, function (err) {
+  // rewrite all relative links to markdown files to point to html equivalents
+  var content = page.body.replace(/\.md"/g, '.html"')
+
+  // stitch together header, content, and footer
+  fs.writeFile(page.path, header + content + footer, function (err) {
     if (err) return console.error(err)
     console.log('built', page.path)
   })
